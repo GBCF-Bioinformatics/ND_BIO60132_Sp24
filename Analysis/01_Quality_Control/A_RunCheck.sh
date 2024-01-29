073ba447a6c39afff134d1518d5e1d3af001a8cb
GBCF-Bioinformatics <152224463+GBCF-Bioinformatics@users.noreply.github.com>
2024-01-29 12:07:50 -0500

    Update A_RunCheck.sh

--- Analysis/01_Quality_Control/A_RunCheck.sh
+++ Analysis/01_Quality_Control/A_RunCheck.sh
@@ -87,7 +87,7 @@ echo "Downloading and indexing genome"
 
       curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000001405.38/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000001405.38.zip" -H "Accept: application/zip"
       mv GCF_000001405.38.zip  ./References/human
-      unzip ../References/human/GCF_000001405.38.zip -d ./References/human/
+      unzip ./References/human/GCF_000001405.38.zip -d ./References/human/
       mv ./References/human/ncbi_dataset/data/GCF_000001405.38/GCF_000001405.38_GRCh38.p12_genomic.fna ./References/human/hsref_GRCh38_p12.fa
       rm ./References/human/GCF_000001405.38.zip
       rm -R ./References/human/ncbi_dataset/
