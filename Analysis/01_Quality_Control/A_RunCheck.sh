56c1c0a14eb14d8a329c8834c77a13fe73c8d744
GBCF-Bioinformatics <152224463+GBCF-Bioinformatics@users.noreply.github.com>
2024-01-29 11:36:02 -0500

    Fixed path to References

--- Analysis/01_Quality_Control/A_RunCheck.sh
+++ Analysis/01_Quality_Control/A_RunCheck.sh
@@ -51,7 +51,7 @@ echo "DONE Checking software"
 echo "Checking for databases"
 
 echo "Checking for adaptors file for trimming"
-if [ -f ../References/adaptors.fa ]; then
+if [ -f ./References/adaptors.fa ]; then
     echo ".....Adaptor files exist"
 else
     echo ".....Please create an adaptors.fa file in the Reference directory for trimming"
@@ -59,39 +59,39 @@ else
 fi
 
 echo "Checking for spike in file for trimming"
-if [ -f ../References/phix_adapters.fa.gz ]; then
+if [ -f ./References/phix_adapters.fa.gz ]; then
     echo ".....Spike in files exist"
 else
-    wget https://github.com/BioInfoTools/BBMap/raw/master/resources/phix_adapters.fa.gz -P ../References
+    wget https://github.com/BioInfoTools/BBMap/raw/master/resources/phix_adapters.fa.gz -P ./References
 fi
 
 
 echo "Checking for rRNA database"
-if [ -f ../References/smr_v4.3_default_db.fasta ]; then
+if [ -f ./References/smr_v4.3_default_db.fasta ]; then
     echo ".....rRNA database exists"
 else
     wget https://github.com/sortmerna/sortmerna/releases/download/v4.3.3/database.tar.gz
-    mv database.tar.gz ../References/
-    tar xfv ../References/database.tar.gz --directory ../References/
+    mv database.tar.gz ./References/
+    tar xfv ./References/database.tar.gz --directory ./References/
 fi
 
 echo "Creating human reference database"
 
 echo "Downloading and indexing genome"
 
-   if [ -d "../References/human" ]; then
+   if [ -d "./References/human" ]; then
       echo ".....human genome already exists"
    else
       echo "pulling human reference - this could take a while"
       mkdir ../References/human
 
       curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_000001405.38/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCF_000001405.38.zip" -H "Accept: application/zip"
-      mv GCF_000001405.38.zip  ../References/human
-      unzip ../References/human/GCF_000001405.38.zip -d ../References/human/
-      mv ../References/human/ncbi_dataset/data/GCF_000001405.38/GCF_000001405.38_GRCh38.p12_genomic.fna ../References/human/hsref_GRCh38_p12.fa
-      rm ../References/human/GCF_000001405.38.zip
-      rm -R ../References/human/ncbi_dataset/
-      rm ../References/human/README.md
+      mv GCF_000001405.38.zip  ./References/human
+      unzip ../References/human/GCF_000001405.38.zip -d ./References/human/
+      mv ./References/human/ncbi_dataset/data/GCF_000001405.38/GCF_000001405.38_GRCh38.p12_genomic.fna ./References/human/hsref_GRCh38_p12.fa
+      rm ./References/human/GCF_000001405.38.zip
+      rm -R ./References/human/ncbi_dataset/
+      rm ./References/human/README.md
 
       wget https://downloads.sourceforge.net/project/prinseq/standalone/prinseq-lite-0.20.4.tar.gz -P ../Software
       tar xvf ../Software/prinseq-lite-0.20.4.tar.gz --directory ../Software
@@ -102,22 +102,22 @@ echo "Downloading and indexing genome"
       perl ../Software/prinseq-lite-0.20.4/prinseq-lite.pl \
       -log \
       -verbose \
-      -fasta ../References/human/hsref_GRCh38_p12.fa \
+      -fasta ./References/human/hsref_GRCh38_p12.fa \
       -min_len 200 \
       -ns_max_p 10 \
       -derep 12345 \
-      -out_good ../References/human/hsref_GRCh38_p12_clean \
+      -out_good ./References/human/hsref_GRCh38_p12_clean \
       -seq_id hsref_GRCh38_p12_ \
       -rm_header \
       -out_bad null
 
-      rm ../References/human/hsref_GRCh38_p12.fa
+      rm ./References/human/hsref_GRCh38_p12.fa
 
       #indexing the genome
       echo "Indexing human genome"
       bbmap.sh \
-      ref=../References/human/hsref_GRCh38_p12_clean.fasta \
-      path=../References/human
+      ref=./References/human/hsref_GRCh38_p12_clean.fasta \
+      path=./References/human
    fi
 
 echo "DONE downloading and indexing genome!"
